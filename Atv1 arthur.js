fef8f91b24d5fe89b4e2ad0f22a47ed6bfd12ce5
Arthur Rocha Lima <99411293+Arthur668@users.noreply.github.com>
2022-05-19 12:17:32 -0300

    Add files via upload

---
+++ Atv1 arthur.js
@@ -0,0 +1,26 @@
+var nomes = ['arthur', 'bruno', 'alex', 'joao', 'bernardo']
+    console.log(nomes)
+
+var salarios = ['1000', '1500', '2000', '2500', '3000', '3500', '4000', '4500']
+    console.log(salarios)
+
+var boolean = [70 > 100, 100 < 50, 50 == 50, 100 == 100]
+    console.log(boolean)
+
+var salarios2 = [20000,30000,40000]
+    console.log(salarios2);
+
+var Produtos = [
+    {
+        maca: 3*0.05,
+        banana: 4*0.05,
+        laranja: 5*0.05,
+    }
+]
+console.log(Produtos)
+
+var matriz = [[9,8,0],[6,5,4],[1,3,4]]
+console.log(matriz[0][0] *2, matriz[1][1] *2, matriz[2][2] *2)
+
+var matriz2 = [[2,4],[3,6]]
+console.log(matriz2[0][0] * matriz2[1][1] - matriz2[0][1] * matriz2[1][0])
